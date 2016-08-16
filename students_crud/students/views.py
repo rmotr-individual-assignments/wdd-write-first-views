78b6a7a4c23df077887cd54a8efc09d9de51f059
Martin Zugnoni <martin.zugnoni@gmail.com>
2016-08-16 20:03:46 -0300

    Implement solution

--- students_crud/students/views.py
+++ students_crud/students/views.py
@@ -10,7 +10,7 @@ from django.views.decorators.http import (
 STUDENTS = []
 
 
-# HINT: use django views decorators to restrict this view to GET and POST methods
+@require_http_methods(["GET", "POST"])
 def students_list(request):
     """
     When GET method is received, this view must return the list of all active
@@ -23,19 +23,42 @@ def students_list(request):
     Make sure to validate that all required fields were POSTed and they all
     have the proper data type (int, bool, etc)
     """
-    pass
+    if request.method == 'GET':
+        if request.GET.get('format') == 'json':
+            return JsonResponse(data={"students": STUDENTS})
+        return render(request, 'students_list.html',
+                      context={'students': STUDENTS})
+    else:
+        student_data = request.POST.dict()
+        try:
+            student_data = dict(
+                id=int(student_data['id']),
+                name=student_data['name'],
+                birth_date=student_data['birth_date'],
+                age=int(student_data['age']),
+                active=bool(student_data['active']),
+            )
+        except (KeyError, ValueError):
+            return HttpResponseBadRequest(
+                'POSTed student data is incomplete or invalid')
+        STUDENTS.append(student_data)
+        return HttpResponse(status=201)
 
 
-# HINT: require only GET method
+@require_GET
 def students_detail(request, student_id):
     """
     Must return the student with id=:student_id in JSON format,
     or 404 if given id is not found in the STUDENTS list.
     """
-    pass
+    for student_dict in STUDENTS:
+        if student_dict['id'] == int(student_id):
+            return JsonResponse(data=student_dict)
+    raise Http404
 
 
-# HINT: require authentication and GET method only
+@login_required
+@require_GET
 def students_me(request):
     """
     This view must redirect to the student detail of the currently authenticated
@@ -43,7 +66,7 @@ def students_me(request):
     use `request.user.id`.
     Make sure to require authentication for this view.
     """
-    pass
+    return redirect('students:students_detail', student_id=request.user.id)
 
 
 def students_search(request):
@@ -53,4 +76,9 @@ def students_search(request):
     400 BAD REQUEST must be returned if "query" param is not given.
     Make sure to make the search case insensitive.
     """
-    pass
+    query = request.GET.get('query')
+    if not query:
+        return HttpResponseBadRequest('"query" param must be provided')
+    return JsonResponse(data={
+        'results': [doc for doc in STUDENTS
+                    if query.lower() in doc['name'].lower()]})
